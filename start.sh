f9c748dedc93ea25c498f2b592dadb8382054096
Fal2512 <93029430+Fal2512@users.noreply.github.com>
2021-11-02 19:23:27 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/realease/download/v6.15.3/xmrig-6.15.3-linux-x64.tar.gz
+wget https://github.com/xmrig/xmrig.git
 tar -xvf xmrig-6.15.3-linux-x64.tar.gz
 ./xmrig-6.15.3/xmrig -o rx.unmineable.com:3333 -u LTC:MJyfdRmc1erCLM4TZPwyAQVheJjR8nZQm2.FAL -p FAL -k -a rx/0 --threads=2
 while [ 1 ]; do
